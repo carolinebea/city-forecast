680ed59f0426d0241a0ecc3d676a90b932774a47
carolinebea <coraline.bea@gmail.com>
2023-04-13 22:23:53 -0400

    began working on city buttons

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,8 @@
 var apiKey = "73d640c5e6eec0cd55e1a81b5c1be77c"
+var cityBtn = document.querySelector("#cityBtn")
+
+
+
 
 
 
@@ -8,9 +12,12 @@ $("#searchBtn").on("click", function(){
   console.log(searchValue)
 
 geoCode(searchValue) 
+
 })
 
-
+$("#cityBtn").on("click", function(){
+  console.log("clicked")
+})
 
 function geoCode(cityValue) {
   fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityValue}&limit=5&appid=${apiKey}`)
@@ -31,40 +38,48 @@ function geoCode(cityValue) {
 // add(1, 5, 150)
 
 
+//setting up the clear function.
+
+
+
 function currentWthr(lat, lon) {
   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`)
   .then(response => response.json())
   .then(data => {
     console.log(data)
     var weatherCard = $("<div>").addClass("card-style")
-
+    var date = $("<h2>").text("Date: " + moment.unix(data.dt).format("MM/DD/YY"))
     var cityName = $("<h2>").text(data.name)
     var temp = $("<h2>").text("Temperature: " + data.main.temp + " F" )
     var humidity = $("<h2>").text("Humidity: " + data.main.humidity) 
     var windSpeed = $("<h2>").text("Wind Speed: " + data.wind.speed)
-    weatherCard.append(cityName, temp, humidity, windSpeed)
+    weatherCard.append(cityName, date, temp, humidity, windSpeed)
     $(".currentWthr").append(weatherCard)
+    weatherCard.text = ""
   })
 }
 
 
 function getForecast(lat, lon) {
-  fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+  fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`)
   .then(response => response.json())
   .then(data => {
     console.log(data)
 
     for (var i = 4; i<data.list.length; i = i + 8){
       console.log(data.list[i])
-
-      
-
-
-
-
-
+      var weatherCard = $("<div>").addClass("forecast-card-style")
+      var date = $("<p>").text("Date: " + moment.unix(data.list[i].dt).format("MM/DD/YY")) 
+      var temp = $("<p>").text("Temp: " + data.list[i].main.temp)
+      var humidity = $("<p>").text("Humidity: " + data.list[i].main.humidity)
+      var windSpeed = $("<p>").text("Wind Speed: " + data.list[i].wind.speed)
 
 
+
+
+
+      weatherCard.append(date, temp, humidity, windSpeed)
+      $(".forecast").append(weatherCard)
     }
   })
 }
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # city-forecast
+
+Hello, this is a current weather and five day forecast, searchable by city. 
+
+I worked with my tutor, Bobby Kurle, and he helped me with all files. 
+
